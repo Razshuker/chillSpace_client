aa49ddc045b892e5c9df6802d8b24cfaae8de079
Razshuker <raz.shuker13@gmail.com>
2023-06-11 14:02:28 +0300

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/header.css'
+import ProfileMenu from './header/profileMenu'
 
 export default function Header() {
     return (
-        <div className='container-fluid bg-info'>
-            <div className="row justify-content-between align-items-center py-4">
-                <div className="col logo">Logo</div>
+        <header className='container-fluid'>
+            <div className="row justify-content-between align-items-center px-4">
+                <div className="col logo">
+                    <img src='images/chillSpaceLogoPNG.png' alt='logo'></img>
+                </div>
                 <div className="nav row col">
                     <nav className="col">
                         <ul className='list-inline d-flex align-items-center'>
@@ -13,9 +17,9 @@ export default function Header() {
                             <li className='me-3'><Link to="/admin/places">PLACES</Link></li>
                         </ul>
                     </nav>
-                    <button className='btn btn-danger col-2 me-4'>logout</button>
+                    <button className='btn btn-danger col-2 me-4'><ProfileMenu /></button>
                 </div>
             </div>
-        </div >
+        </header >
     )
 }
