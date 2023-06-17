4e1db8c96dda8e57b90084b9f2d8d849a3fb089f
Razshuker <raz.shuker13@gmail.com>
2023-06-17 19:06:11 +0300

    finish signup

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,53 +1,70 @@
 import React from 'react'
 import '../css/signup.css'
 import { useForm } from "react-hook-form"
+import { CgProfile } from "react-icons/cg";
+import { API_URL, doApiMethod } from '../services/apiService';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Signup() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, getValues } = useForm();
+    const nav = useNavigate();
 
 
-    const onSub = () => {
-
+    const onSub = async (_data) => {
+        _data.full_name = _data.first_name + " " + _data.last_name;
+        delete _data.first_name;
+        delete _data.last_name;
+        delete _data.confirm_password;
+        console.log(_data);
+        const url = API_URL + '/users';
+        const user = await doApiMethod(url, "POST", _data);
+        if (user._id) {
+            alert("user add");
+            nav("/login");
+        }
     }
 
     return (
-        <div style={{ backgroundImage: 'url("images/sign-upBG.jpg")' }} className='sign-up container_fluid'>
-            <h1>SIGN UP</h1>
-            <div className="d-flex align-items-center">
-
-
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-
-                        </div>
-                        <form onSubmit={handleSubmit(onSub)} className='row col-md-8' >
-                            <div className="col-md-6">
-                                <input placeholder='First name' {...register("first_name", { required: true, minLength: 2 })} className="form-control" type="text" />
-                                {errors.first_name && <div className="text-danger">* Enter valid first_name</div>}
-                                <input placeholder='Last name' {...register("last_name", { required: true, minLength: 2 })} className="form-control" type="text" />
-                                {errors.last_name && <div className="text-danger">* Enter valid last_name</div>}
-                                <input placeholder='Email' {...register("email", { required: true, minLength: 2 })} className="form-control" type="email" />
-                                {errors.email && <div className="text-danger">* Enter valid email</div>}
-                                <input placeholder='Phone' {...register("phone", { required: true, minLength: 2 })} className="form-control" type="tel" />
-                                {errors.phone && <div className="text-danger">* Enter valid phone</div>}
-                            </div>
-                            <div className="col-md-6">
-                                <input placeholder='Nickname' {...register("nickname", { required: true, minLength: 2 })} className="form-control" type="text" />
-                                {errors.nickname && <div className="text-danger">* Enter valid nickname</div>}
-                                <input placeholder='City' {...register("city", { required: true, minLength: 2 })} className="form-control" type="text" />
-                                {errors.city && <div className="text-danger">* Enter valid city</div>}
-                                <input placeholder='Password' {...register("password", { required: true, minLength: 2 })} className="form-control" type="password" />
-                                {errors.password && <div className="text-danger">* Enter valid password</div>}
-                                <input placeholder='Confirm password' {...register("confirm_password", { required: true, minLength: 2 })} className="form-control" type="password" />
-                                {errors.confirm_password && <div className="text-danger">* Enter valid confirm_password</div>}
-                            </div>
-                            <button className='sign-up_btn'>Sign up</button>
-                        </form>
+        <div style={{ backgroundImage: 'url("images/sign-upBG.jpg")' }} className='sign-up container_fluid d-flex align-items-center'>
+            <h1 className='col-12'>SIGN UP</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <CgProfile style={{ fontSize: "16em", color: "rgb(117, 100, 89)" }} />
+                        <input type='file' className='input_upload' />
                     </div>
+                    <form onSubmit={handleSubmit(onSub)} className='row col-md-8' >
+                        <div className="col-md-6">
+                            <input placeholder='First name' {...register("first_name", { required: true, minLength: 2 })} className="form-control" type="text" />
+                            {errors.first_name && <div className="text-danger">* Enter valid first_name</div>}
+                            <input placeholder='Last name' {...register("last_name", { required: true, minLength: 2 })} className="form-control" type="text" />
+                            {errors.last_name && <div className="text-danger">* Enter valid last_name</div>}
+                            <input placeholder='Email' {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })} className="form-control" type="email" />
+                            {errors.email && <div className="text-danger">* Enter valid email</div>}
+                            <input placeholder='Phone' {...register("phone", { required: true, minLength: 2 })} className="form-control" type="tel" />
+                            {errors.phone && <div className="text-danger">* Enter valid phone</div>}
+                        </div>
+                        <div className="col-md-6">
+                            <input placeholder='Nickname' {...register("nickname", { required: true, minLength: 2 })} className="form-control" type="text" />
+                            {errors.nickname && <div className="text-danger">* Enter valid nickname</div>}
+                            <input placeholder='City' {...register("location", { required: true, minLength: 2 })} className="form-control" type="text" />
+                            {errors.location && <div className="text-danger">* Enter valid city</div>}
+                            <input placeholder='Password' {...register("password", { required: true, minLength: 2 })} className="form-control" type="password" />
+                            {errors.password && <div className="text-danger">* Enter valid password</div>}
+                            <input placeholder='Confirm password' {...register("confirm_password", {
+                                required: true, validate: (value) => {
+                                    const { password } = getValues();
+                                    return password === value;
+                                }
+                            })} className="form-control" type="password" />
+                            {errors.confirm_password && <div className="text-danger">* Password aren't match</div>}
+                        </div>
+                        <button className='sign-up_btn'>Sign up</button>
+                    </form>
                 </div>
             </div>
-
         </div>
+
     )
 }
