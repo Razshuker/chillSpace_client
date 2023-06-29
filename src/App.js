25cac0d4f24cf7b52c5d4e6bfffbba8ba263de49
Razshuker <raz.shuker13@gmail.com>
2023-06-29 12:58:33 +0300

    fix the local storge

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React from 'react'
 import AppRoutes from './appRoutes';
 import { MyContext } from './context/myContext';
 import { useUser } from './hooks/useUser';
+import { TOKEN_KEY } from './services/apiService';
 
 function App() {
-
   const { userInfo } = useUser();
+
+  const clearLocalStorge = () => {
+    localStorage.removeItem(TOKEN_KEY);
+    console.log("cleared");
+  }
+  // delete the local storge every 10 hours (when the token is invalid)
+  setInterval(clearLocalStorge, 36000000)
 
   return (
     <MyContext.Provider value={{ userInfo }}>
