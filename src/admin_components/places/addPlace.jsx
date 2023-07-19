4ac634672be7eab3da782cbba69556a294cc8f9c
noyNosrati <noynosrati@gmail.com>
2023-07-19 16:54:06 +0300

    deldete consolelog

--- src/admin_components/places/addPlace.jsx
+++ src/admin_components/places/addPlace.jsx
@@ -73,7 +73,6 @@ export default function AddPlace() {
             delete placeData['open_hours - Saturday'];
             delete placeData['lat'];
             delete placeData['lon'];
-            console.log(placeData);
             doApiPost(placeData);
         } catch (error) {
             console.log(error);
@@ -129,7 +128,7 @@ export default function AddPlace() {
                 <input {...register("city", { required: true, minLength: 2 })} className="form-control" type="text" />
                 {errors.city && <div className="text-danger">* Enter a valid city</div>}
                 <label className="pt-3 pb-1">phone</label>
-                <input {...register("phone", { minLength: 2 })} className="form-control" type="text" />
+                <input {...register("phone", {required: false, minLength: 2 })} className="form-control" type="text" />
                 {errors.phone && <div className="text-danger">* Enter a valid phone</div>}
                 <label className="pt-3 pb-1">latitude</label>
                 <input {...register("lat", { required: true, minLength: 2 })} className="form-control" type="text" />
