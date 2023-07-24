27b76e4b56e5942d13f168bc5ba308d884a8ddd9
Razshuker <raz.shuker13@gmail.com>
2023-07-24 21:14:54 +0300

    fix add post

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -20,7 +20,12 @@ export default function Signup() {
             delete _data.first_name;
             delete _data.last_name;
             delete _data.confirm_password;
-            _data.img_url = await uploadImage(fileRef);
+            if (fileRef.current.files.length > 0) {
+                _data.img_url = await uploadImage(fileRef);
+            } else {
+                _data.img_url = "";
+            }
+            // _data.img_url = await uploadImage(fileRef);
             const url = API_URL + '/users';
             const user = await doApiMethod(url, "POST", _data);
             if (user._id) {
