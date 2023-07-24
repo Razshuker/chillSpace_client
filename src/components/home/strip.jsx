569cdb44d62f91704989c9e8c2ef0229c84fc7eb
Razshuker <raz.shuker13@gmail.com>
2023-07-24 22:37:37 +0300

    css strip

--- src/components/home/strip.jsx
+++ src/components/home/strip.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import '../../css/home.css'
+import React from 'react';
+import { BsSun } from "react-icons/bs";
+import '../../css/home.css';
+
 
 export default function Strip() {
     return (
         <div className='strip'>
-            <video className="video d-lg-block d-none" autoPlay loop muted>
-                <source src={"images/strip.mp4"} type='video/mp4' />
-            </video>
+            <div className="video-container">
+                <video className="video d-lg-block d-none" autoPlay loop muted>
+                    <source src={"images/strip.mp4"} type='video/mp4' />
+                </video>
+            </div>
             <div className='image d-lg-none d-block'></div>
+            <div className="text-overlay">
+                <BsSun className='sunIcon' />
+                <h2>YOUR PLACE</h2>
+                <h3>TO FIND YOUR CHILL</h3>
+            </div>
         </div>
-    )
+    );
 }
