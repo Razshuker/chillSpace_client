7492dc43fa8bb1155a7c0d6a0e83b9e5970174ad
noyNosrati <noynosrati@gmail.com>
2023-07-27 14:25:26 +0300

    sort places by area in progress

--- src/components/places/sortPlaces.jsx
+++ src/components/places/sortPlaces.jsx
@@ -3,9 +3,14 @@ import SearchForm from './searchForm'
 import { useState } from 'react'
 import { useEffect } from 'react';
 import { API_URL, doApiGet } from '../../services/apiService';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export default function SortPlaces({ setShowSort, isShowSort }) {
     const [types,setTypes] = useState([]);
+    const [arArea,setArArea] = useState([]);
+    const nav = useNavigate();
+    const [query] = useSearchParams();
+
 
     useEffect(()=> {
         doApiTypes()
@@ -16,35 +21,49 @@ export default function SortPlaces({ setShowSort, isShowSort }) {
         const data = await doApiGet(url);
         setTypes(data);
     }
-
+    const onSelectedArea = (_area) => {
+        const updatedAreaArr = [...arArea];
+        const isIn = updatedAreaArr.includes(_area);
+        if (isIn) {
+          const index = updatedAreaArr.indexOf(_area);
+          updatedAreaArr.splice(index, 1);
+        } else {
+          updatedAreaArr.push(_area);
+        }
+        setArArea(updatedAreaArr);
+        const searchParams = new URLSearchParams(query);
+        searchParams.set("area", updatedAreaArr.join(",")); 
+        nav("?" + searchParams.toString()); 
+      };
+  
     return (
         <div className='sortMenu border h-auto'>
             <SearchForm setShowSort={setShowSort} isShowSort={isShowSort} />
             <div className='mt-5'>
-                <h4 className='display-6 text-center'>location</h4>
+                <h4 className='display-6 text-center'>Area</h4>
                 <div className='m-4'>
-                    <input type="checkbox" name="scales" />
+                    <input  onClick={()=>{onSelectedArea("north")}} type="checkbox" name="scales" />
                     <label className='ms-1' htmlFor="north">North</label>
                 </div>
                 <div className='m-4'>
-                    <input type="checkbox" name="scales" />
+                    <input onClick={()=>{onSelectedArea("south")}} type="checkbox"  />
                     <label className='ms-1' htmlFor="south">South</label>
                 </div>
                 <div className='m-4'>
-                    <input type="checkbox" name="scales" />
+                    <input  onClick={()=>{onSelectedArea("center")}} type="checkbox"  />
                     <label className='ms-1' htmlFor="center">Center</label>
                 </div>
                 <div className='m-4'>
-                    <input type="checkbox" name="scales" />
+                    <input  onClick={()=>{onSelectedArea("jerusalem")}} type="checkbox"  />
                     <label className='ms-1' htmlFor="jerusalem">Jerusalem</label>
                 </div>
 
 
-                <h4 className='display-6 text-center'>what to do</h4>
+                <h4 className='display-6 text-center'>What to do</h4>
                 {types.map(item => {
                     return(
-                <div className='m-4'>
-                    <input type="checkbox" name="scales" />
+                <div className='m-4' key={item._id}>
+                    <input type="checkbox"  />
                     <label className='ms-1' htmlFor={item.type_name}>{item.type_name}</label>
                 </div>
                     )
