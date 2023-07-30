5241d9756061808ace81b6f752c398706b40dc28
noyNosrati <noynosrati@gmail.com>
2023-07-30 17:49:12 +0300

    done

--- src/components/places/searchForm.jsx
+++ src/components/places/searchForm.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import { FaSistrix } from "react-icons/fa";
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { BiSlider, BiX } from "react-icons/bi";
+import {BsFillTrash3Fill} from "react-icons/bs"
 import '../../css/places.css'
 import { useEffect } from 'react';
 
@@ -41,16 +42,24 @@ export default function SearchForm({ setShowSort, isShowSort, setPage }) {
         <div className='container-fluid'>
             <div className="container mt-5">
                 <div className="row align-items-center justify-content-between w-100">
+                    <div className='d-flex align-items-center col-3 p-0'>
                     {isShowSort ?
-                        <BiX onClick={onClickSort} className='h2 col-2 sort_icon' />
+                    <div className='h1 ps-2-1 col-2 sort_icon me-4'>
+                        <BiX onClick={onClickSort} />
+                    </div>
                         :
-                        <BiSlider onClick={onClickSort} className='h2 col-2 sort_icon' />
+                        <BiSlider onClick={onClickSort} className='h2 col-2 sort_icon m-0' />
                     }
+                    <div>
+                     <button onClick={onCleanSearch} className='cleanBtn col-auto '><BsFillTrash3Fill/></button>
+                    </div>
+
+                    </div> 
                     <div className='d-flex align-items-center col-9'>
                         <input onKeyDown={onEnterSearch} ref={inputRef} placeholder='search for place...' type="text" className='input_search me-2 col-10' />
                         <button onClick={onSearch} className='btn_search col-auto'><FaSistrix className='search_icon' /></button>
                     </div>
-                    <button onClick={onCleanSearch} className='cleanBtn col-auto mt-4'>clean search</button>
+                    {/* <button onClick={onCleanSearch} className='cleanBtn col-auto mt-4'>clean search</button> */}
                 </div>
             </div>
         </div>
