e010bdab3b201683bae260c067ab30e47edb34c7
Razshuker <raz.shuker13@gmail.com>
2023-07-30 12:31:53 +0300

    fix scroll x

--- src/components/home/categoryItem.jsx
+++ src/components/home/categoryItem.jsx
@@ -6,7 +6,7 @@ export default function CategoryItem({ item }) {
     const img_url = item.img_url || "/images/categoryDefault.jpg";
     return (
         <div style={{ backgroundImage: `url(${img_url})` }} className="cat_box">
-            <a href={`?category=${item.name}`}>
+            <a href={`/places?category=${item.name}`}>
                 <div className="category-name">{item.name}</div>
             </a>
         </div>
