7198c9f923101b7fb9372852e177475eaad38985
Razshuker <raz.shuker13@gmail.com>
2023-08-02 09:36:57 +0300

    fix css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -26,22 +26,22 @@ export default function Header() {
     return (
         <header className='container-fluid'>
             <div className='container'>
-                <div className=" d-flex justify-content-between align-items-center border">
-                    <div className="col-auto logo border">
+                <div className=" d-flex justify-content-between align-items-center">
+                    <div className="col-auto logo">
                         <img onClick={() => {
                             nav("/")
                         }} src='images/chillSpaceLogoPNG.png' alt='logo' />
                     </div>
                     <div className='row'>
                         <div className={isBurgerOpen ? "col-0" : "row col-auto align-items-center "}>
-                            <nav className={isBurgerOpen ? "burgerShow d-lg-flex nav border " : "nav d-lg-flex justify-content-end border"}>
+                            <nav className={isBurgerOpen ? "burgerShow d-lg-flex nav " : "nav d-lg-flex justify-content-end"}>
                                 <ul className='list-inline m-0 d-lg-flex '>
                                     {isBurgerOpen ?
                                         <li className='d-lg-none'>
-                                            {userInfo.img_url ? <img src={userInfo.img_url} alt='profile' className='profile-img' /> : 
-                                             <AccountCircle className='profile_btn' fontSize='large' />
-                                            // <Link to='/login'>Login</Link>
-                                          
+                                            {userInfo.img_url ? <img src={userInfo.img_url} alt='profile' className='profile-img' /> :
+                                                <AccountCircle className='profile_btn' fontSize='large' />
+                                                // <Link to='/login'>Login</Link>
+
                                             }
 
                                         </li> : <></>
@@ -52,9 +52,9 @@ export default function Header() {
                                 </ul>
                             </nav>
                         </div>
-                        <div className='col-2 border d-none d-lg-block'><ProfileMenu /></div>
+                        <div className='col-2 d-none d-lg-block'><ProfileMenu /></div>
                     </div>
-                    <div className="col-auto d-flex justify-content-end align-items-center d-lg-none border">
+                    <div className="col-auto d-flex justify-content-end align-items-center d-lg-none">
                         <CiMenuBurger onClick={onToggleBurger} className='burger p-0' />
                     </div>
                 </div>
