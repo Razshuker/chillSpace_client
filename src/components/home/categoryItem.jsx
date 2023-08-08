4d87ffe41d427a39677aa18508383abbeeb5e289
Razshuker <raz.shuker13@gmail.com>
2023-08-08 11:06:29 +0300

    .

--- src/components/home/categoryItem.jsx
+++ src/components/home/categoryItem.jsx
@@ -6,7 +6,7 @@ export default function CategoryItem({ item }) {
     const img_url = item.img_url || "/images/categoryDefault.jpg";
     return (
         <div style={{ backgroundImage: `url(${img_url})` }} className="cat_box">
-            <a href={`/places?cats=${item.name}`}>
+            <a href={`/places?cats=${item.category_code}`}>
                 <div className="category-name">{item.name}</div>
             </a>
         </div>
