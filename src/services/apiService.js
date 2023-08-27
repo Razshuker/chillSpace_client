7242ba89b1fd1ea9fd73db43f488436a96504973
Razshuker <raz.shuker13@gmail.com>
2023-08-27 17:20:25 +0300

    change server

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const API_URL = "http://localhost:3001";
+// export const API_URL = "http://localhost:3001";
+export const API_URL = "https://chillspace.cyclic.cloud";
 export const TOKEN_KEY = "token";
 
 export const doApiGet = async (_url) => {
